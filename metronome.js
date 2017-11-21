4f32fb4abec30543501f31e06e83f7f338dc54b6
Aidan Taylor <aidantwothousand@gmail.com>
2017-11-21 10:48:57 -0700

    styling, bpm slider

--- metronome.js
+++ metronome.js
@@ -1,31 +1,36 @@
 let audio = new Audio('metronome.wav')
 let toggleButton = document.getElementById("toggle")
-let bpmField = document.getElementById("bpm")
-let metronomeActive = false
-
-bpmField.value = 60
+let bpmHeader = document.getElementById("bpmHeader")
+let bpmSlider = document.getElementById("bpmSlider")
+bpmHeader.innerHTML = bpmSlider.value
 
 function metronome() {
 
+	let timeout;
+
 	function tick() {
 		let dt = Date.now() - self.expected
-		console.log(60000/self.bpm-dt)
+		if (self.active) {
+			timeout = setTimeout(() => {tick(); audio.currentTime = 0; audio.play()}, 60000/self.bpm-dt)
+		}
 		self.expected += 60000/self.bpm
-		let timeout = setTimeout(() => {audio.currentTime = 0; audio.play(); tick()}, 60000/self.bpm - dt)
 	}
 
 	const self = {
 		start : function start() {
 			self.active = true
-			self.expected = Date.now() + 60000/self.bpm
-			console.log(self.expected)
+			self.expected = Date.now()
+			audio.currentTime = 0
+			audio.play()
 			tick()
 		},
 		stop : function stop() {
 			self.active = false
-			clearTimeout(timeout)
+			if (timeout) {
+				clearTimeout(timeout)
+			}
 		},
-		bpm : bpmField.value,
+		bpm : bpmSlider.value,
 		expected : null,
 		active : false
 	}
@@ -34,11 +39,10 @@ function metronome() {
 
 m = metronome()
 
-bpmField.addEventListener("keyup", (event) => {
-	if (event.keyCode == 13) {
-		m.bpm = bpmField.value
-	}
-})
+bpmSlider.oninput = () => {
+	m.bpm = bpmSlider.value
+	bpmHeader.innerHTML = bpmSlider.value
+}
 
 toggleButton.onclick = () => {
 	if (m.active) {
